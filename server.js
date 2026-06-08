eef2b4c1dea27cc838544c46a143b097d6baacf0
mehanathank <mehanathank06@gmail.com>
2026-06-08 13:30:25 +0530

    fix express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 
 // Enhanced body parsing with limits
 app.use(express.json({ limit: '50mb' }));
